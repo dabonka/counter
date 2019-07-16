9e73cc976d6035824e6806c0108645e829964248
Roman Belik <dabonka@yandex.ru>
2019-07-16 18:13:19 +0300

    Updates

---
+++ precache-manifest.00a8498fd8a7030b7a7da0c2c11e6bfd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ef8164e101ee529339341cf58271ee",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/counter/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "3b82246ef68e869b3f81",
+    "url": "/counter/static/js/main.39da84f2.chunk.js"
+  },
+  {
+    "revision": "395f42efa7c50e4ddc67",
+    "url": "/counter/static/js/runtime~main.16ddd954.js"
+  }
+]);
